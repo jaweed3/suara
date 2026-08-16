78940e40cd1292258249247006180d65d1509380
jaweed3 <wedjaw22@gmail.com>
2026-08-16 16:09:48 +0700

    Home: feed curhat eksplisit saat belum login (fix tidak muncul)

--- src/app/views/Home.svelte
+++ src/app/views/Home.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+  import {pubkey} from "@welshman/app"
   import {makeTagFeed} from "@welshman/feeds"
   import Feeds from "src/app/views/Feeds.svelte"
   import {makeFeed} from "src/domain"
+  import {userFollows} from "src/engine"
 
   export let feed = null
   export let topic = null
+
+  // ponytail: tampilkan curhatan kalau belum login & belum follow, biar feed gak kosong.
+  $: resolvedFeed = topic
+    ? makeFeed({definition: makeTagFeed("#t", topic)})
+    : feed ||
+      (!$pubkey && $userFollows.size === 0
+        ? makeFeed({definition: makeTagFeed("#t", "curhat")})
+        : null)
 </script>
 
-<Feeds feed={topic ? makeFeed({definition: makeTagFeed("#t", topic)}) : feed} />
+<Feeds feed={resolvedFeed} />
